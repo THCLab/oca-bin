dbf667721cd2619391fc161537f02a6671397dba
Edyta Pawlak <edyta.pawlakk@gmail.com>
2024-06-12 12:40:11 +0200

    feat: process arrays in mapping function

--- src/mapping.rs
+++ src/mapping.rs
@@ -49,11 +49,11 @@ fn handle_attr(
         NestedAttrType::Reference(RefValue::Said(said)) => {
             handle_reference(said, &name, facade, dep_graph)
         }
-        NestedAttrType::Value(value) => {
+        NestedAttrType::Value(_) => {
             vec![name]
         }
-        NestedAttrType::Array(_) => todo!(),
-        NestedAttrType::Null => todo!(),
+        NestedAttrType::Array(attr) => handle_attr(name, *attr, facade, dep_graph),
+        NestedAttrType::Null => vec![name],
     }
 }
 
@@ -79,55 +79,59 @@ fn handle_reference(
         .collect()
 }
 
-#[test]
-fn test_handle_references() {
-    use std::io::Write;
-    let tmp_dir = tempdir::TempDir::new("db").unwrap();
+#[cfg(test)]
+mod tests {
+    use crate::{dependency_graph::DependencyGraph, get_oca_facade, mapping::mapping};
+    use std::{fs::File, io::Write};
 
-    let mut facade = get_oca_facade(tmp_dir.path().to_path_buf());
+    #[test]
+    fn test_handle_references() {
+        let tmp_dir = tempdir::TempDir::new("db").unwrap();
 
-    let oca_file0 = r#"ADD ATTRIBUTE name=Text number=Numeric"#.to_string();
+        let mut facade = get_oca_facade(tmp_dir.path().to_path_buf());
 
-    // Value oca bundle
-    let oca_bundle0 = facade.build_from_ocafile(oca_file0.clone()).unwrap();
-    let digest0 = oca_bundle0.said.unwrap();
+        let oca_file0 = r#"ADD ATTRIBUTE name=Text number=Numeric"#.to_string();
 
-    let oca_file1 = format!(
-        "ADD ATTRIBUTE person=refs:{}\nADD ATTRIBUTE like_cats=Boolean",
-        digest0.to_string()
-    );
+        // Value oca bundle
+        let oca_bundle0 = facade.build_from_ocafile(oca_file0.clone()).unwrap();
+        let digest0 = oca_bundle0.said.unwrap();
 
-    // Reference oca bundle
-    let oca_bundle1 = facade.build_from_ocafile(oca_file1.clone()).unwrap();
-    let digest1 = oca_bundle1.said.unwrap();
+        let oca_file1 = format!(
+            "ADD ATTRIBUTE person=refs:{}\nADD ATTRIBUTE like_cats=Boolean",
+            digest0.to_string()
+        );
 
-    let oca_file2 = format!(
-        "ADD ATTRIBUTE cat_lover=refs:{}\nADD ATTRIBUTE favorite_cat=Text",
-        digest1.to_string()
-    );
+        // Reference oca bundle
+        let oca_bundle1 = facade.build_from_ocafile(oca_file1.clone()).unwrap();
+        let digest1 = oca_bundle1.said.unwrap();
 
-    // Reference to Reference oca bundle
-    let oca_bundle2 = facade.build_from_ocafile(oca_file2.clone()).unwrap();
-    let digest2 = oca_bundle2.said.unwrap();
+        let oca_file2 = format!(
+            "ADD ATTRIBUTE cat_lover=refs:{}\nADD ATTRIBUTE favorite_cat=Text",
+            digest1.to_string()
+        );
 
-    // Build temporary directory with test ocafiles.
-    let list = [
-        ("first.ocafile", oca_file0),
-        ("second.ocafile", oca_file1),
-        ("third.ocafile", oca_file2),
-    ];
+        // Reference to Reference oca bundle
+        let oca_bundle2 = facade.build_from_ocafile(oca_file2.clone()).unwrap();
+        let digest2 = oca_bundle2.said.unwrap();
 
-    let mut paths = vec![];
-    for (name, contents) in list {
-        let path = tmp_dir.path().join(name);
-        let mut tmp_file = File::create(&path).unwrap();
-        writeln!(tmp_file, "{}", contents).unwrap();
-        paths.push(path)
-    }
-    let dependency_graph = DependencyGraph::from_paths(tmp_dir.path(), paths).unwrap();
-    let o = mapping(digest2, &facade, &dependency_graph).unwrap();
+        // Build temporary directory with test ocafiles.
+        let list = [
+            ("first.ocafile", oca_file0),
+            ("second.ocafile", oca_file1),
+            ("third.ocafile", oca_file2),
+        ];
 
-    let expected_json = r#"{
+        let mut paths = vec![];
+        for (name, contents) in list {
+            let path = tmp_dir.path().join(name);
+            let mut tmp_file = File::create(&path).unwrap();
+            writeln!(tmp_file, "{}", contents).unwrap();
+            paths.push(path)
+        }
+        let dependency_graph = DependencyGraph::from_paths(tmp_dir.path(), paths).unwrap();
+        let o = mapping(digest2, &facade, &dependency_graph).unwrap();
+
+        let expected_json = r#"{
   "digest": "EJFw2ZOSK0wdiKTmB0dvovdi9Y20Xb5Aye4DuvmW_qKT",
   "cat_lover.like_cats": "",
   "cat_lover.person.name": "",
@@ -135,50 +139,61 @@ fn test_handle_references() {
   "favorite_cat": ""
 }"#;
 
-    let actual_json = serde_json::to_string_pretty(&o).unwrap();
-    assert_eq!(expected_json, actual_json)
+        let actual_json = serde_json::to_string_pretty(&o).unwrap();
+        assert_eq!(expected_json, actual_json)
+    }
+
+    #[test]
+    fn test_handle_array() {
+        let tmp_dir = tempdir::TempDir::new("db").unwrap();
+
+        let mut facade = get_oca_facade(tmp_dir.path().to_path_buf());
+
+        let oca_file1 = r#"ADD ATTRIBUTE name=Text number=Numeric"#.to_string();
+
+        // Value oca bundle
+        let oca_bundle0 = facade.build_from_ocafile(oca_file1.clone()).unwrap();
+        let digest0 = oca_bundle0.said.unwrap();
+
+        let oca_file2 = format!("ADD ATTRIBUTE person=refs:{}", digest0.to_string());
+
+        // Reference oca bundle
+        let person_oca_bundle = facade.build_from_ocafile(oca_file2.clone()).unwrap();
+        let person_bundle_said = person_oca_bundle.said.unwrap();
+
+        // Array of references oca bundle
+        let oca_file3 = format!(
+            "ADD ATTRIBUTE many_persons=Array[refs:{}]",
+            person_bundle_said.to_string()
+        );
+
+        let many_persons_bundle = facade.build_from_ocafile(oca_file3.clone()).unwrap();
+        let many_person_bundle_digest = many_persons_bundle.said.unwrap();
+
+        // Build temporary directory with test ocafiles.
+        let list = [
+            ("first.ocafile", oca_file1),
+            ("second.ocafile", oca_file2),
+            ("third.ocafile", oca_file3),
+        ];
+        let mut paths = vec![];
+        for (name, contents) in list {
+            let path = tmp_dir.path().join(name);
+            let mut tmp_file = File::create(&path).unwrap();
+            writeln!(tmp_file, "{}", contents).unwrap();
+            paths.push(path)
+        }
+
+        let dependency_graph = DependencyGraph::from_paths(tmp_dir.path(), paths).unwrap();
+        let o = mapping(
+            many_person_bundle_digest.clone(),
+            &facade,
+            &dependency_graph,
+        )
+        .unwrap();
+
+        let actual_json = serde_json::to_string_pretty(&o).unwrap();
+        // assert_eq!(expected_json, actual_json);
+        println!("{}", actual_json);
+    }
 }
-
-// #[test]
-//  fn test_handle_array() {
-//     let tmp_dir = tempdir::TempDir::new("db").unwrap();
-
-//     let mut facade = get_oca_facade(tmp_dir.path().to_path_buf());
-
-//     // Array of values
-//     let oca_file0 = "ADD ATTRIBUTE list=Array[Numeric] name=Text".to_string();
-
-//     // Reference oca bundle
-//     let array_bundle = facade.build_from_ocafile(oca_file0.clone()).unwrap();
-//     let array_bundle_said = array_bundle.said.unwrap();
-
-//     let presentation = handle_flatten(array_bundle_said.clone(), &facade).unwrap();
-
-//     let oca_file1 = r#"ADD ATTRIBUTE name=Text number=Numeric"#.to_string();
-
-//     // Value oca bundle
-//     let oca_bundle0 = facade.build_from_ocafile(oca_file1.clone()).unwrap();
-//     let digest0 = oca_bundle0.said.unwrap();
-
-//     let presentation = handle_flatten(digest0.clone(), &facade).unwrap();
-
-//     let oca_file1 = format!("ADD ATTRIBUTE person=refs:{}", digest0.to_string());
-
-//     // Reference oca bundle
-//     let person_oca_bundle = facade.build_from_ocafile(oca_file1.clone()).unwrap();
-//     let person_bundle_said = person_oca_bundle.said.unwrap();
-
-//     let presentation = handle_flatten(person_bundle_said.clone(), &facade).unwrap();
-
-//     // Array of references oca bundle
-//     let oca_file2 = format!(
-//         "ADD ATTRIBUTE many_persons=Array[refs:{}]",
-//         person_bundle_said.to_string()
-//     );
-
-//     let many_persons_bundle = facade.build_from_ocafile(oca_file2.clone()).unwrap();
-//     let many_person_bundle_digest = many_persons_bundle.said.unwrap();
-
-//     let presentation = handle_flatten(many_person_bundle_digest, &facade).unwrap();
-
-//  }
